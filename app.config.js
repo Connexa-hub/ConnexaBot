5bdc9aed405399b9f2d45166d646b26014857e0f
Connexa-hub <akinsofth@gmail.com>
2025-09-17 11:20:40 +0100

    Add EAS projectId

--- app.config.js
+++ app.config.js
@@ -1,30 +1,34 @@
-export default {
-  expo: {
-    name: "ConnexaBotApp",
-    slug: "ConnexaBotApp",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
-    newArchEnabled: true,
-    splash: {
-      image: "./assets/splash-icon.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
+export default ({ config }) => ({
+  ...config,
+  name: "ConnexaBotApp",
+  slug: "ConnexaBotApp",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    ios: {
-      supportsTablet: true
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: "874f0bba-eefb-4aae-a8cd-d28a8ca9128a", // ✅ Added manually
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      },
-      edgeToEdgeEnabled: true,
-      predictiveBackGestureEnabled: false
-    },
-    web: {
-      favicon: "./assets/favicon.png"
-    }
-  }
-};
+  },
+});
